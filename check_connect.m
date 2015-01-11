c4d497f113fa21f6a61e89b2cd260f24bb125ef2
Aaron Diaz <aad1974@gmail.com>
2015-01-10 19:37:56 -0800

    minor

--- check_connect.m
+++ check_connect.m
@@ -1,16 +1,20 @@
-function edst=check_connect(coef,per)
-%function edst=check_connect(coef,per)
+function [edst,h]=check_connect(coef,per,plt)
+%function [edst,h]=check_connect(coef,per,plt)
 %
 %IN: coef is a nX2 matrix of euclidean coordinates
 %    per is a vector of percentages, at which to downsample the ensemble
+%    plt is a bool, if true then will generate a plot and return handle
 %
 %OUT: edst is a cell array of size length(per), edst{i} is a beta
 %distribution object fitted from the edge probabilities down-sampled to a
 %percentage of per(i)
+%     h is a plot handle if plt is true, else it is -1
 
+h=-1;
 edst={};
 n=size(coef,1);
-for i=1:length(per)
+m=length(per);
+for i=1:m
     idx=randperm(n);
     idx=idx(1:floor(per(i)*n));
     c=coef(sort(idx),:);
@@ -27,3 +31,20 @@ for i=1:length(per)
     pd=fitdist(x,'beta');
     edst{i}=pd;
 end
+if plt 
+    M=zeros(2,m);
+    N=M;
+    for i=1:m
+        M=paramci(edst{i});
+        tb=makedist('beta','a',M(1,1),'b',M(2,2));
+        N(1,i)=mean(tb);
+        tb=makedist('beta','a',M(2,1),'b',M(1,2));
+        N(2,i)=mean(tb);
+    end
+    h=figure;
+    set(h,'color','w');
+    bar(N','stacked');
+end
+    
+        
+        
