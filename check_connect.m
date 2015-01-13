0cc4ca6ed0a6f44787b1f37e6821caf69f67cf16
Aaron Diaz <aad1974@gmail.com>
2015-01-13 09:46:40 -0800

    minor

--- check_connect.m
+++ check_connect.m
@@ -1,8 +1,9 @@
-function [edst,h]=check_connect(coef,per,plt)
-%function [edst,h]=check_connect(coef,per,plt)
+function [md,h]=check_connect(coef,nit,ep,plt)
+%function [md,h]=check_connect(coef,nit,ep,plt)
 %
 %IN: coef is a nX2 matrix of euclidean coordinates
-%    per is a vector of percentages, at which to downsample the ensemble
+%    nit is the number of iterations of MCMC to run
+%    ep is the amount of perturbation to perform on each sample
 %    plt is a bool, if true then will generate a plot and return handle
 %d
 %OUT: edst is a cell array of size length(per), edst{i} is a beta
@@ -11,42 +12,35 @@ function [edst,h]=check_connect(coef,per,plt)
 %     h is a plot handle if plt is true, else it is -1
 
 h=-1;
-edst={};
+md=[];
 n=size(coef,1);
-m=length(per);
-for i=1:m
-    e=gabrielGraph(coef);
-    idx=randperm(size(e,1));
-    idx=idx(1:floor(per(i)*n));
+%compute mean for initial input data
+e=gabrielGraph(coef);
+!rm -f tmp.pairs
+f=fopen('tmp.pairs','w');
+for j=1:n
+    fprintf(f,'%i\t',e(j,1));
+    fprintf(f,'%i\n',e(j,2));
+end
+[status,result]=system('./fitHRG -f tmp.pairs');
+x=textscan(result,'%n','Delimiter','\n');
+x=x{1}; x=x(end-n:end);
+md=[md;mean(x)];
+%compute means for perturbed data
+for i=1:nit
+    ptb=(1-2*rand(size(coef)))*ep;
+    e=gabrielGraph(coef+ptb);
     !rm -f tmp.pairs
     f=fopen('tmp.pairs','w');
-    for j=1:length(idx)
-        fprintf(f,'%i\t',e(idx(j),1));
-        fprintf(f,'%i\n',e(idx(j),2));
+    for j=1:n
+        fprintf(f,'%i\t',e(j,1));
+        fprintf(f,'%i\n',e(j,2));
     end
     [status,result]=system('./fitHRG -f tmp.pairs');
     x=textscan(result,'%n','Delimiter','\n');
     x=x{1}; x=x(end-n:end);
-    pd=fitdist(x,'beta');
-    edst{i}=pd;
+    md=[md;mean(x)];
 end
 if plt 
-    M=zeros(2,m);
-    N=M;
-    for i=1:m
-        M=paramci(edst{i});
-        tb=makedist('beta','a',M(1,1),'b',M(2,2));
-        N(1,i)=mean(tb);
-        tb=makedist('beta','a',M(2,1),'b',M(1,2));
-        N(2,i)=mean(tb);
-        t(i)=mean(edst{i});
-    end
-    h=figure;
-    set(h,'color','w');
-    bar(N','stacked');
-    hold on
-    plot([1:m],t,'g*')
+    h=boxplot(md)
 end
-    
-        
-        
